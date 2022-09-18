1cb822c7e1ac5e9107b12d42142c83bf87adb681
Caracole <francoismoulin37@gmail.com>
2022-09-18 23:10:00 +0200

    missing the whole javascript part

---
+++ index.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const score = document.querySelector(".score");
+const yourScore = document.getElementById("yourScore");
+
+btn.addEventListener("click", () => {
+  console.log(yourScore);
+});
