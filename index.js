55c15bd4283826e68dbccff82e96ed064c83e790
Caracole <francoismoulin37@gmail.com>
2022-09-25 17:52:12 +0200

    htmlelement.hidden test struggle

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const btn = document.getElementById("btn");
+const thankU = document.getElementById("thankUstate");
+const rate = document.getElementById("ratingState");
+
 const score1 = document.querySelector(".score1");
 const yourScore = document.getElementById("yourScore");
-const thankU = document.querySelector(".thankUstate");
 
-btn.addEventListener("click", () => {
-  thankU.style.visibility = "visible";
-});
+btn.addEventListener(
+  "click",
+  () => {
+    rate.hidden = true;
+    thankU.hidden = false;
+  },
+  false
+);
