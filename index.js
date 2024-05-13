a5dae4704f45a92d7a3eed13ba374f875c474018
C4r4cole <c4r4cole.chevalerie@gmail.com>
2024-05-13 11:40:23 +0200

    project finished

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const btn = document.getElementById("btn");
-const thankU = document.getElementById("thankUstate");
-const rate = document.getElementById("ratingState");
+const btn = document.getElementById('btn');
+const thankU = document.getElementById('thankUstate');
+const rate = document.getElementById('ratingState');
 
-const score1 = document.querySelector(".score1");
-const yourScore = document.getElementById("yourScore");
+const score1 = document.getElementById('score1');
+const score2 = document.getElementById('score2');
+const score3 = document.getElementById('score3');
+const score4 = document.getElementById('score4');
+const score5 = document.getElementById('score5');
+
+const yourScore = document.getElementById('yourScore');
+
+function isRadioBtnChecked() {
+	if (score1.checked) {
+		yourScore.textContent = '1';
+	}
+
+	if (score2.checked) {
+		yourScore.textContent = '2';
+	}
+
+	if (score3.checked) {
+		yourScore.textContent = '3';
+	}
+
+	if (score4.checked) {
+		yourScore.textContent = '4';
+	}
+
+	if (score5.checked) {
+		yourScore.textContent = '5';
+	}
+}
 
 btn.addEventListener(
-  "click",
-  () => {
-    rate.hidden = true;
-    thankU.hidden = false;
-  },
-  false
+	'click',
+	() => {
+		rate.hidden = true;
+		thankU.hidden = false;
+		isRadioBtnChecked();
+	},
+	false
 );
